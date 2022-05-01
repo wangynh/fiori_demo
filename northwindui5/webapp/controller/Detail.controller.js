85033e4b9a59e09bcaf9a7bb40b8d649c3a72970
Yanhu WANG <wangynh@schaeffler.com>
2022-05-01 14:45:32 +0000

    changes

--- northwindui5/webapp/controller/Detail.controller.js
+++ northwindui5/webapp/controller/Detail.controller.js
@@ -1,22 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox, MessageToast) {
         "use strict";
 
         return Controller.extend("fiori.northwindui5.controller.Detail", {
             onInit: function () {
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.getRoute("detail").attachMatched(this._onRouteMatched, this);
+                console.log("detail init...");
             },
             _onRouteMatched: function(oEvent){
                 var oArgs, oView;
-                oArgs = oEvent.getPrameter("arguments");
-                oView = oEvent.getView();
-                oVeiw.bindElement({
+                oArgs = oEvent.getParameter("arguments");
+              //  oView = oEvent.getView();
+                oView = this.getView();
+                oView.bindElement({
                     path: "/Products(" + oArgs.productId + ")",
                     events: {
                         dataRequested: function () {
@@ -31,7 +35,24 @@ sap.ui.define([
             handleNavButtonPress: function (evt) {
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("home");
-            }
+            },
+
+            handleOrder: function (evt) {
+                // show confirmation dialog
+                var bundle = this.getView().getModel("i18n").getResourceBundle();
+                MessageBox.confirm(
+                    bundle.getText("OrderDialogMsg"),
+                    function (oAction) {
+                        if (MessageBox.Action.OK === oAction) {
+                            // notify user
+                            var successMsg = bundle.getText("OrderDialogSuccessMsg");
+                            MessageToast.show(successMsg);
+                            // TODO call proper service method and update model (not part of this tutorial)
+                        }
+                    },
+                    bundle.getText("OrderDialogTitle")
+                );
+            }            
 
         });
     });
